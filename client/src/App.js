cd4a73b0433302f9ab86138f93a5b5552337ce2c
John Obertubbesing <oberthewhat@gmail.com>
2020-06-05 10:48:36 -0500

    started on nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
+import Nav from './components/nav'
 
 function App() {
   return (
     <div>
-      webDev interview Q's<br/>
-      ya better not snooze<br/>
-      you got nothing to lose<br/>
-      on this opportuze<br/>
-      <h3>SIGN UP TODAY! or youll be gay, but thats okay! </h3>
-      <button>$199<sup>99</sup></button>
+      <Nav/>
     </div>
   );
 }
